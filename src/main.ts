0b956a3b34e2e95d7b9b937c8eee32989c1b68e5
a20688392 <a20688392@gmail.com>
2023-03-18 23:57:20 +0800

    feat: setup sentry

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { INestApplication } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as Sentry from "@sentry/node";
 import * as dotenv from "dotenv";
+import { SentryInterceptor } from "sentry/sentry.interceptor";
 
 import { AppModule } from "./app.module";
 import { Environment } from "./config/env.validation";
@@ -11,6 +13,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   if (process.env.NODE_ENV !== Environment.Production) {
     setupSwagger(app);
+  }
+  if (process.env.NODE_ENV !== Environment.Local) {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      tracesSampleRate: 1.0,
+      environment: process.env.NODE_ENV,
+    });
+    app.useGlobalInterceptors(new SentryInterceptor());
   }
   await app.listen(3000);
 }
