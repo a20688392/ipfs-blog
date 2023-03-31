be83b35392d54b71da78f321c38a96d495ff4786
蔡侄宇 <a20688392@gmail.com>
2023-03-31 16:44:03 +0800

    feature: userlogin response add userdata

--- src/auth/exceptions/check-notfound-error.exception.ts
+++ src/auth/exceptions/check-notfound-error.exception.ts
@@ -6,7 +6,7 @@ export class CheckNotFoundError {
     description: "HTTP 回應代碼",
     example: "404",
   })
-  public readonly StatusCode: number;
+  public readonly statusCode: number;
 
   @ApiProperty({
     type: "string",
--- src/auth/respose/generate-token.respose.ts
+++ src/auth/respose/generate-token.respose.ts
@@ -6,7 +6,7 @@ export class GenerateTokenRespose {
     description: "HTTP 回應代碼",
     example: "201",
   })
-  public readonly StatusCode: number;
+  public readonly statusCode: number;
 
   @ApiProperty({
     type: "string",
@@ -15,4 +15,42 @@ export class GenerateTokenRespose {
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiYWRkcmVzcyI6IjB4RUZhNEFiYWM3RmVkQjhGMDUxNGJlRTcyMTJkYzE5RDUyM0REMzA4OSIsImVtYWlsIjoiQW5keUBnbWFpbC5jb20iLCJpYXQiOjE2Nzk3NDM4NzUsImV4cCI6MTY3OTgzMDI3NX0.sm2aIH1SMIpEnEYlISxgt_VYyuNVnXI3sITA0oXrtCs",
   })
   public readonly accessToken: string;
+
+  @ApiProperty({
+    type: "array",
+    description: "User 資料",
+    items: {
+      properties: {
+        id: {
+          description: "使用者 ID。  \n",
+          type: "string",
+        },
+        name: {
+          description: "使用者 名稱。  \n",
+          type: "string",
+        },
+        address: {
+          description: "使用者 錢包地址。  \n",
+          type: "string",
+        },
+        email: {
+          description: "使用者 信箱。  \n",
+          type: "string",
+        },
+        photo: {
+          description: "使用者 圖片。  \n",
+          type: "string",
+        },
+      },
+    },
+    example: {
+      id: 1,
+      name: "Jhon",
+      address: "0x264D6BF791f6Be6F001A95e895AE0a904732d473",
+      emai: "jhon@gmail.com",
+      photo:
+        "https://www.gravatar.com/avatar/490311069a0a679192286d1ab009ae9a?s=800&d=identicon",
+    },
+  })
+  public readonly userData: string[];
 }
